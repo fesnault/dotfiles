3040ccb8918ff529cdfc289fac13b8da19af8d96
Wolfgang <mail@weirdrescue.pw>
2021-08-20 11:32:08 +0200

    Remove exa from fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,17 +4,17 @@ export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 source $HOME/.config/fish/shortcuts.fish
 source $HOME/.config/fish/colors.fish
-set -gx PATH $HOME/.local/bin /usr/local/bin /opt/chromium /opt/android-sdk/platform-tools $PATH
+set -gx PATH $HOME/.local/bin /usr/local/bin /opt/chromium /opt/android-sdk/platform-tools /opt/homebrew/bin $PATH
 
 alias v=$EDITOR
-alias ls='exa'
-alias la='exa -la'
+alias la='ls -la'
 alias df='df -h'
 alias du='du -ch'
-alias pass="kpcli -kdb ~/Documents/pass.kdbx"
 alias ipp='curl ipinfo.io/ip'
-alias sudo='sudo '
-alias playlist-dl="youtube-dl -cio '%(autonumber)s-%(title)s.%(ext)s'"
-alias yt='youtube-dl'
-alias ytv='yt -f bestvideo'
-alias yta='yt -f bestaudio'
+alias yh='youtube-dl --continue --no-check-certificate --format=bestvideo+bestaudio[ext=m4a]/best --merge-output-format=mp4'
+alias yd='youtube-dl --continue --no-check-certificate --format=bestvideo+bestaudio --exec="ffmpeg -i {} -c:v dnxhd -profile:v dnxhr_hq -vf fps=25/1,format=yuv422p -c:a pcm_s16le {}.mov && rm {}"'
+alias ya='youtube-dl --continue --no-check-certificate --format=bestaudio -x --audio-format wav'
+
+# Blog commands
+alias generate='rm -f dst/.files && ssg5 src dst "Wolfgang\'s Blog" "http://notthebe.ee"'
+alias deploy='rsync --rsync-path "sudo -u www-data rsync" -avP --delete dst/ snowfall:/var/www/blog'
